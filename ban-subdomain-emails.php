005d2da2087ee8022f43c731175f2aea1fa7825b
Jamie Chong <jamie@hippocurious.com>
2016-04-13 11:33:57 -0700

    make public

---
+++ ban-subdomain-emails.php
@@ -0,0 +1,99 @@
+<?php
+/*
+Plugin Name: Ban Subdomain Emails
+Plugin URI: https://github.com/jamiechong/ban-subdomain-emails
+Description: Prevent people from registering with emails that contain a subdomain
+Author: Jamie Chong
+Version: 1.0.0
+Author URI: http://jamiechong.ca
+License: GPLv2
+*/
+
+/*  Copyright 2016 Jamie Chong
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+class BanSubdomainEmails {
+    protected $blocked;
+    protected $optionKey = 'bse_blocked_list';
+    private static $_instance = null;
+
+    private static function instance() {
+        if (self::$_instance === null) {
+            self::$_instance = new BanSubdomainEmails();
+        }
+    }
+
+    public static function init() {
+        self::instance();
+    }
+
+    public function __construct() {
+        $this->blocked = $this->get_blocked_list();
+        add_action('register_post', array($this, 'on_register'), 9999, 3);    // handle action very late to give other spam blocking plugins a chance to block
+        add_filter('plugin_action_links_'.plugin_basename(__FILE__), array($this, 'plugin_info'), 10, 2);
+        add_action('wp_ajax_ban_subdomain_emails', array($this, 'report'));
+    }
+
+    // TODO don't consider country specific TLDs as subdomains such as .co.fr, .co.uk
+    public function on_register($username, $email, $errors) {
+        list($name, $domain) = explode('@', $email);
+        $count = substr_count($domain, '.');
+
+        if ($count > 1) {
+            $this->block_email($email);
+            $errors->add('invalid_email', __('Invalid email.'));    
+        }
+    }
+
+    public function plugin_info($links) {
+        $url = wp_nonce_url(admin_url('admin-ajax.php?action=ban_subdomain_emails'), 'ban_subdomain_emails');
+
+        array_unshift($links, '<a href="'.$url.'" target="_blank">Blocked '.count($this->blocked).' emails</a>');
+        return $links;
+    }
+
+    public function get_blocked_list() {
+        $list = get_option($this->optionKey);
+        if (!$list) {
+            $list = array();
+        }
+        return $list;
+    }
+
+    public function block_email($email) {
+        $list = $this->get_blocked_list();
+        if (!in_array($email, $list)) {
+            $list[] = $email;    
+        }
+        update_option($this->optionKey, $list, false);
+    }
+
+    public function report() {
+        if(empty($_GET['action']) || !is_user_logged_in() || !check_admin_referer($_GET['action'])) {
+            wp_die(__('You do not have sufficient permissions to access this page.'));
+        }
+        $list = $this->get_blocked_list();
+        echo '<pre>';
+        echo implode("\n", $list);
+        echo '</pre>';
+        exit;
+    }
+
+}
+
+add_action('init', 'BanSubdomainEmails::init');
---
+++ readme.txt
@@ -0,0 +1,25 @@
+=== Ban Subdomain Emails ===
+Contributors: Ipstenu
+Tags: email, ban, registration, spam, akismet, all-in-one, anti-captcha, anti-spam, antispam, antispambot, blacklist, block spam, bot, bots, e-mail, email, email address, login, no captcha, nocaptcha, protect, protection, register, registration spam, robot, robots, signup, signup spam, spam blocker, spam filter, spambot, user registration spam, 
+Requires at least: 4.0
+Tested up to: 4.5
+Stable tag: trunk
+License: GPLv2
+
+Prevent people from registering with emails that contain a subdomain
+
+== Description ==
+
+A lot of registration spam comes from emails with a subdomain. Spammers control a domain such as buymyjunk.com and then attempt to register on your site with emails such as irenemwd@foster2.buymyjunk.com and jakemgm@foster7.buymyjunk.com. This plugin simply blocks all registrations using a subdomain.
+
+Reputable users will have a normal email address from a reasonable domain like @gmail.com, @mylegitbusiness.com, @outlook.com, @myschool.edu, etc.
+
+There is no configuration and no setup. It simply just works. The plugin does log all blocked registrations so you can find potential false-positives. 
+
+==Changelog==
+
+=  1.0.0 =
+* released 2016-04-13 *
+
+* First public version.
+
